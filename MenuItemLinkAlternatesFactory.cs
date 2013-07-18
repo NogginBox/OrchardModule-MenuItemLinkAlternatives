d971c491e88210c7fedcc837599d656cf9e6c02d
Richard Garside <729381+NogginBox@users.noreply.github.com>
2013-07-18 16:05:25 +0100

    Working version of module.

---
+++ MenuItemLinkAlternatesFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Orchard.DisplayManagement.Implementation;
+
+namespace NogginBox.MenuItemLinkAlternates
+{
+	public class MenuItemLinkAlternatesFactory : ShapeDisplayEvents
+	{
+		public override void Displaying(ShapeDisplayingContext context) {
+			context.ShapeMetadata.OnDisplaying(displayedContext => {
+				String zoneName;
+				switch (displayedContext.ShapeMetadata.Type) {
+					case "Parts_MenuWidget":
+						zoneName = displayedContext.Shape.ContentItem.WidgetPart.Zone;
+						displayedContext.Shape.Menu.Zone = zoneName;
+						break;
+					case "MenuItem":
+						zoneName = displayedContext.Shape.Menu.Zone;
+						displayedContext.Shape.Zone = zoneName;
+						break;
+					case "MenuItemLink":
+						zoneName = displayedContext.Shape.Zone;
+						const String shapeName = "MenuItemLink";
+						//var contentTypeName = "Notsureyet";//contentItem.ContentType;
+						displayedContext.ShapeMetadata.Alternates.Add(shapeName + "__" + zoneName);
+						//displayedContext.ShapeMetadata.Alternates.Add(shapeName + "__" + contentTypeName + "__" + zoneName);
+						break;
+				}
+			});
+		}
+	}
+}
