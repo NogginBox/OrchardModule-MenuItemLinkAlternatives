6f54b680ebd14d2d46e3490c0fc6dc261a7ecfed
NogginBox <NogginBox@localhost>
2013-07-22 21:11:59 +0100

    Updated to add zone to more menu parts.

--- MenuItemLinkAlternatesFactory.cs
+++ MenuItemLinkAlternatesFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Orchard.DisplayManagement.Implementation;
 
 namespace NogginBox.MenuItemLinkAlternates
@@ -7,21 +8,28 @@ namespace NogginBox.MenuItemLinkAlternates
 	{
 		public override void Displaying(ShapeDisplayingContext context) {
 			context.ShapeMetadata.OnDisplaying(displayedContext => {
-				String zoneName;
+				var alternates = displayedContext.ShapeMetadata.Alternates;
 				switch (displayedContext.ShapeMetadata.Type) {
 					case "Parts_MenuWidget":
-						zoneName = displayedContext.Shape.ContentItem.WidgetPart.Zone;
+						var zoneName = displayedContext.Shape.ContentItem.WidgetPart.Zone;
 						displayedContext.Shape.Menu.Zone = zoneName;
 						break;
+					case "Menu":
+						AddAlternates(alternates, displayedContext.Shape.Zone, "Menu");
+						break;
+					case "MenuItem":
+						AddAlternates(alternates, displayedContext.Shape.Parent.Zone, "MenuItem");
+						break;
 					case "MenuItemLink":
-						zoneName = displayedContext.Shape.Parent.Zone;
-						const String shapeName = "MenuItemLink";
-						//var contentTypeName = "Notsureyet";//contentItem.ContentType;
-						displayedContext.ShapeMetadata.Alternates.Add(shapeName + "__" + zoneName);
-						//displayedContext.ShapeMetadata.Alternates.Add(shapeName + "__" + contentTypeName + "__" + zoneName);
+						AddAlternates(alternates, displayedContext.Shape.Parent.Zone, "MenuItemLink");
 						break;
 				}
 			});
+		}
+
+		private static void AddAlternates(IList<String> alternateCollection, String zoneName, String shapeName) {
+			alternateCollection.Add(shapeName + "__" + zoneName);
+			//alternateCollection.Add(shapeName + "__" + contentTypeName + "__" + zoneName);
 		}
 	}
 }
